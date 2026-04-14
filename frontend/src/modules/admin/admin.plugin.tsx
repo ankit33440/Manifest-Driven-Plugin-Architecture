f310b0bb5921e8ad366581b564e364a8706e60c3
ankit33440 <ankit33440@gmail.com>
2026-04-14 16:54:58 +0530

    feat(admin): remove registration of unused AdminDashboardPage component

--- frontend/src/modules/admin/admin.plugin.tsx
+++ frontend/src/modules/admin/admin.plugin.tsx
@@ -1,12 +1,10 @@
 import { componentRegistry } from '../../core/component-registry';
 import AdminDashboard from './components/AdminDashboard';
-import AdminDashboardPage from './pages/AdminDashboardPage';
 import AdminUsersPage from './pages/AdminUsersPage';
 import AdminAuditPage from './pages/AdminAuditPage';
 
 export default function register() {
   componentRegistry.register('AdminDashboard', AdminDashboard);
-  componentRegistry.register('AdminDashboardPage', AdminDashboardPage);
   componentRegistry.register('AdminUsersPage', AdminUsersPage);
   componentRegistry.register('AdminAuditPage', AdminAuditPage);
 }
