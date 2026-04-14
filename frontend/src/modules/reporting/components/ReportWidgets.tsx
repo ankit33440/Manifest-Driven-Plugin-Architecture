19811279ae9bfa34b283e54be6ce10cd91104b61
ankit33440 <ankit33440@gmail.com>
2026-04-14 16:53:51 +0530

    feat: remove unused credits and marketplace modules, including related components and pages

--- frontend/src/modules/reporting/components/ReportWidgets.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ReportWidgets = () => {
-  return (
-    <div>ReportWidgets</div>
-  )
-}
-
-export default ReportWidgets
--- frontend/src/modules/reporting/reporting.plugin.tsx
+++
@@ -1,8 +0,0 @@
-import ReportDashboardPage from './pages/ReportDashboardPage';
-import { componentRegistry } from '../../core/component-registry';
-import ReportWidgets from './components/ReportWidgets';
-// Important: Must be exported as `default`
-export default function register() {
-  componentRegistry.register('ReportDashboardPage', ReportDashboardPage);
-  componentRegistry.register('ReportWidgets', ReportWidgets);
-}
